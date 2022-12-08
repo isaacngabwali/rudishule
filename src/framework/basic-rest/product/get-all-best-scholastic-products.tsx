47feead2d9303404156c8c9a5268ea05f2d1016e
isaacngabwali <31367648+isaacngabwali@users.noreply.github.com>
2022-12-08 16:29:05 +0300

    header cor rules

--- src/framework/basic-rest/product/get-all-best-scholastic-products.tsx
+++ src/framework/basic-rest/product/get-all-best-scholastic-products.tsx
@@ -7,7 +7,7 @@ import axios from "axios";
 export const fetchBestScholasticProducts = async ({ queryKey }: any) => {
   const api = axios.create({
     baseURL: 'http://rsbase.fanitehub.com/api/v1',
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: { "Access-Control-Allow-Origin": "*" },
   });
   const [_key, _params] = queryKey;
   const { data } = await api.get("products");
