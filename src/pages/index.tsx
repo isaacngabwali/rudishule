9aefc16da282d7c764c2370de5c4bd415f388e2c
isaacngabwali <31367648+isaacngabwali@users.noreply.github.com>
2022-12-08 15:50:42 +0300

    scholastics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import DownloadAppsTwo from '@components/common/download-apps-two';
 import BundleGrid from '@components/bundle/bundle-grid-two';
 import CollectionGrid from '@components/common/collection-grid';
-import BestSellerGroceryProductFeed from '@components/product/feeds/best-seller-grocery-product-feed';
+import BestSellerScholasticFeed from '@components/product/feeds/best-scholastics';
 import { bundleDataThree as bundle } from '@framework/static/bundle';
 import { GetStaticProps } from 'next';
 import Seo from '@components/seo/seo';
@@ -15,20 +15,18 @@ import { fetchCategories } from '@framework/category/get-all-categories';
 import { fetchBestSellerGroceryProducts } from '@framework/product/get-all-best-seller-grocery-products';
 import { fetchPopularProducts } from '@framework/product/get-all-popular-products';
 import { LIMITS } from '@framework/utils/limits';
-import BannerGridTwo from '@components/common/banner-grid-two';
 import BannerHeroGrid from '@components/common/banner-hero-grid';
 import { bannersGridHero as bannersHero } from '@framework/static/banner';
-import { elegantBannerGrid as banners } from '@framework/static/banner';
 import FeatureCarousel from '@components/common/featured-carousel';
-import PopularProductWithBestDeals from '@components/product/popular-product-with-best-deals';
+import PopularProductWithBestDeals from '@components/product/featured-scholastic';
 
 export default function Home() {
   return (
     <>
       <Seo
-        title="Elegant"
-        description="Fastest E-commerce template built with React, NextJS, TypeScript, React-Query and Tailwind CSS."
-        path="/elegant"
+        title="Rudi Shule"
+        description="Online Sholastic Back to school Shopping."
+        path="/"
       />
 
       <Container>
@@ -37,23 +35,17 @@ export default function Home() {
           className="my-3 md:my-4 lg:mt-0 lg:mb-5 xl:mb-6"
         />
         <FeatureCarousel />
-        <BestSellerGroceryProductFeed className="mb-12 lg:mb-14 xl:mb-16 2xl:mb-20" />
+        <BestSellerScholasticFeed className="mb-12 lg:mb-14 xl:mb-16 2xl:mb-20" />
         <BundleGrid
           className="mb-12 lg:mb-14 xl:mb-16 2xl:mb-20"
           data={bundle}
         />
-        <PopularProductWithBestDeals />
-        <BannerGridTwo
-          data={banners}
-          className="mb-12 lg:mb-14 xl:mb-16 2xl:mb-20"
-          girdClassName="xl:gap-5 3xl:gap-7"
-        />
-      </Container>
-
-      <CollectionGrid
+        <CollectionGrid
         headingPosition="center"
         className="pb-1 lg:pb-0 mb-12 lg:mb-14 xl:mb-16 2xl:mb-20"
       />
+        <PopularProductWithBestDeals />
+      </Container>
       <DownloadAppsTwo />
     </>
   );
