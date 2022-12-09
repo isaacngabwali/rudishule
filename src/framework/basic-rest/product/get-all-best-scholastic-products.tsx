84a5c3538a5fe9aaab18affaa15aa98dbc90b92f
isaacngabwali <31367648+isaacngabwali@users.noreply.github.com>
2022-12-09 15:19:27 +0300

    cor

--- src/framework/basic-rest/product/get-all-best-scholastic-products.tsx
+++ src/framework/basic-rest/product/get-all-best-scholastic-products.tsx
@@ -7,7 +7,6 @@ import axios from "axios";
 export const fetchBestScholasticProducts = async ({ queryKey }: any) => {
   const api = axios.create({
     baseURL: 'https://rsbase.fanitehub.com/api/v1',
-    headers: { "Access-Control-Allow-Origin": "https://rs.fanitehub.com/" },
   });
   const [_key, _params] = queryKey;
   const { data } = await api.get("products");
